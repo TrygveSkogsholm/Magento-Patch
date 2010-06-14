765037d2613a07b2bbdda97e8c0df00ae33a1d8b
Trygve Skogsholm <Kolansuka@hotmail.com>
2010-06-14 16:42:10 -0400

    Made picking list smaller, (comments not working though)

--- app/code/core/Mage/Sales/Model/Order/Pdf/PickingList.php
+++ app/code/core/Mage/Sales/Model/Order/Pdf/PickingList.php
@@ -14,27 +14,8 @@
 
  This is going to be heavy on the comments because I am a newbe with this stuff.
  */
-class Mage_Sales_Model_Order_Pdf_PickingList extends Mage_Sales_Model_Order_Pdf_Abstract
+class Mage_Sales_Model_Order_Pdf_PickingList extends MDN_Orderpreparation_Model_Pdf_Pdfhelper
 {
-	//public function getPdf($invoices = array())
-	/*public function getPdf()
-	{
-	$this->_beforeGetPdf();
-	$this->_initRenderer('invoice');
-
-	$pdf = new Zend_Pdf();
-	$this->_setPdf($pdf);
-	$style = new Zend_Pdf_Style();
-	$this->_setFontBold($style, 10);
-
-	$page = $pdf->newPage(Zend_Pdf_Page::SIZE_A4);
-	$pdf->pages[] = $page;
-
-	$page->drawText('hello world', 35, $this->y, 'UTF-8');
-
-	return $pdf;
-	}*/
-
 
 	public function getPdf($order = array())
 	{
@@ -63,6 +44,11 @@ class Mage_Sales_Model_Order_Pdf_PickingList extends Mage_Sales_Model_Order_Pdf_
 		// 800 is the top (including page margins ect 840 is absolute top). oh and 600 wide
 		$this->y = 790;
 
+		// some nice color cpu saving
+		$white = new Zend_Pdf_Color_GrayScale(1);
+		$black = new Zend_Pdf_Color_GrayScale(0);
+		$grey = new Zend_Pdf_Color_GrayScale(0.5);
+		$darkGrey = new Zend_Pdf_Color_GrayScale(0.2);
 		/*
 		 * Here starts the drawing of the header info section
 		 */
@@ -72,18 +58,18 @@ class Mage_Sales_Model_Order_Pdf_PickingList extends Mage_Sales_Model_Order_Pdf_
 		$page->drawText(Mage::helper('sales')->__('Order Date: ') . Mage::helper('core')->formatDate($order->getCreatedAtStoreDate(), 'medium', false), 40, $this->y + 7, 'UTF-8');
 
 		//line setup don't forget it!
-		$page->setLineColor(new Zend_Pdf_Color_GrayScale(0.0));
+		$page->setLineColor($black);
 		$page->setLineWidth(1);
 
 		//Going down to headers of first row
 		//$this->y -=11;
 
 		//headers are white in dark background this effects rectangle
-		$page->setFillColor(new Zend_Pdf_Color_GrayScale(0.5));
+		$page->setFillColor($grey);
 
 		//header box
 		//$page->drawRectangle(25, $this->y-4, 570, $this->y +11);
-		$page->drawRectangle(25, $this->y-160, 570, $this->y -175);
+		$page->drawRectangle(380, $this->y-140, 570, $this->y -155);
 		$page->drawLine(25, $this->y-4, 570, $this->y-4);
 
 		//NUMBER easy read boxes
@@ -94,7 +80,7 @@ class Mage_Sales_Model_Order_Pdf_PickingList extends Mage_Sales_Model_Order_Pdf_
 		$page->drawLine(390, $this->y-4, 390, $this->y-60);
 
 		$page->drawLine(25, $this->y-60, 570, $this->y-60);
-		$page->setFillColor(new Zend_Pdf_Color_GrayScale(0.0));
+		$page->setFillColor($black);
 
 		//easy read order lines
 		$page->setLineColor(new Zend_Pdf_Color_GrayScale(0.6));
@@ -102,7 +88,7 @@ class Mage_Sales_Model_Order_Pdf_PickingList extends Mage_Sales_Model_Order_Pdf_
 		$page->drawLine(274, $this->y-5, 274, $this->y-59);
 		$page->drawLine(304, $this->y-5, 304, $this->y-59);
 		$page->drawLine(335, $this->y-5, 335, $this->y-59);
-		$page->setLineColor(new Zend_Pdf_Color_GrayScale(0.0));
+		$page->setLineColor($black);
 
 		$this->y -=50;
 
@@ -123,16 +109,34 @@ class Mage_Sales_Model_Order_Pdf_PickingList extends Mage_Sales_Model_Order_Pdf_
 			$this->_setFontRegular($page, 26);
 			$page->drawText(substr($order->getShippingAddress()->getRegion(), 0, 14), 395, $this->y+8, 'UTF-8');
 		}
-		$this->_setFontRegular($page, 30);
+		$this->_setFontItalic($page, 8);
 		//second row end first
 		$this->y -=40;
 
 		//customer ID
-		$page->drawText($order->getcustomer_id(), 306, $this->y, 'UTF-8');
-		$page->drawLine(380, $this->y+30, 380, $this->y-10);
+		$page->drawText('Account #', 306, $this->y+20, 'UTF-8');
+		$this->_setFontRegular($page, 30);
+		$page->drawText($order->getcustomer_id(), 306, $this->y-4, 'UTF-8');
+		$page->drawLine(380, $this->y+30, 380, $this->y-40);
 
-		// Customer last name
-		$page->drawText(substr($order->getCustomerLastname(), 0, 18), 25, $this->y, 'UTF-8');
+		// Customer full last name
+
+		$actualWidth = 275;
+		$initialWidth = $this->widthForStringUsingFontSize2($order->getCustomerName(), $page->getFont(), $page->getFontSize());
+
+		if ($initialWidth >= $actualWidth)
+		{
+			$FontSize = $page->getFontSize();
+			$fontType = $page->getFont();
+			$changingString = $order->getCustomerName();
+			while ($this->widthForStringUsingFontSize2($changingString, $fontType, $FontSize) >= $actualWidth)
+			{
+				$FontSize -=1;
+			}
+			$this->_setFontRegular($page, $FontSize);
+		}
+
+		$page->drawText($order->getCustomerName(), 25, $this->y, 'UTF-8');
 		$page->drawLine(304, $this->y+30, 304, $this->y-10);
 
 		// Shipping Speed
@@ -145,47 +149,49 @@ class Mage_Sales_Model_Order_Pdf_PickingList extends Mage_Sales_Model_Order_Pdf_
 
 		//starting from the top on this one (because of comments box)
 		$this->y -=10;
-		$page->drawRectangle(25, $this->y, 380, $this->y -60,Zend_Pdf_Page::SHAPE_DRAW_STROKE);
-		$page->drawLine(380, $this->y-30, 570, $this->y-30);
-		$page->drawLine(380, $this->y-60, 570, $this->y-60);
+		$page->drawRectangle(25, $this->y, 380, $this->y -95,Zend_Pdf_Page::SHAPE_DRAW_STROKE);
+		$page->drawLine(380, $this->y-20, 570, $this->y-20);
+		$page->drawLine(380, $this->y-40, 570, $this->y-40);
 
 
 		$this->_setFontRegular($page, 15);
 		$page->drawText($order->getComment(), 27, $this->y, 'UTF-8');
-		$this->_setFontRegular($page, 30);
-		$page->drawText($order->formatPriceTxt($order->getShippingAmount()), 383, $this->y-26, 'UTF-8');
-		$page->drawText($order->formatPriceTxt($order->getSubtotal()), 383, $this->y-56, 'UTF-8');
+		$this->_setFontRegular($page, 18);
+		$page->drawText('Shipping: '.$order->formatPriceTxt($order->getShippingAmount()), 383, $this->y-16.5, 'UTF-8');
+		$page->drawText('SubTotal: '.$order->formatPriceTxt($order->getSubtotal()), 383, $this->y-36.5, 'UTF-8');
 
 		//pick check row
-		$this->y -=75;
+		$this->y -=55;
 
 		//headers are white in dark background this effects text
-		$page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
+		$page->setFillColor($white);
 		$this->_setFontRegular($page, 10);
 
-		//Destination header
-		$page->drawText('Picked By', 73, $this->y+4, 'UTF-8');
-		$page->drawText('Checked By', 209, $this->y+4, 'UTF-8');
-		$page->drawText('Packed By', 345, $this->y+4, 'UTF-8');
-		$page->drawText('Labeled By', 481, $this->y+4, 'UTF-8');
+		//pick check header
+		$page->drawText('Picked', 385, $this->y+4, 'UTF-8');
+		$page->drawText('Checked', 431, $this->y+4, 'UTF-8');
+		$page->drawText('Packed', 482, $this->y+4, 'UTF-8');
+		$page->drawText('Labeled', 526, $this->y+4, 'UTF-8');
 
-		$page->drawRectangle(25, $this->y+15, 161.25, $this->y -60,Zend_Pdf_Page::SHAPE_DRAW_STROKE);
-		$page->drawRectangle(161.25, $this->y+15, 297.5, $this->y -60,Zend_Pdf_Page::SHAPE_DRAW_STROKE);
-		$page->drawRectangle(297.5, $this->y+15, 433.75, $this->y -60,Zend_Pdf_Page::SHAPE_DRAW_STROKE);
-		$page->drawRectangle(433.75, $this->y+15, 570, $this->y -60,Zend_Pdf_Page::SHAPE_DRAW_STROKE);
+		$page->drawRectangle(380, $this->y+15, 427.5, $this->y -40,Zend_Pdf_Page::SHAPE_DRAW_STROKE);
+		$page->drawRectangle(427.5, $this->y+15, 475, $this->y -40,Zend_Pdf_Page::SHAPE_DRAW_STROKE);
+		$page->drawRectangle(475, $this->y+15, 522.5, $this->y -40,Zend_Pdf_Page::SHAPE_DRAW_STROKE);
+		$page->drawRectangle(522.5, $this->y+15, 570, $this->y -40,Zend_Pdf_Page::SHAPE_DRAW_STROKE);
 
-		$this->y -=78;
+		$this->y -=60;
 
 		//Item Table Headers
-		$page->setFillColor(new Zend_Pdf_Color_GrayScale(0.4));
-		$this->_setFontRegular($page, 18);
-		$page->drawText('Shelf', 70, $this->y, 'UTF-8');
-		$page->drawText('Qty', 158, $this->y, 'UTF-8');
-		$page->drawText('SKU', 220, $this->y, 'UTF-8');
-		$page->drawText('Product', 310, $this->y, 'UTF-8');
-		$page->drawLine(25, $this->y-6, 570, $this->y-6);
+		$page->setFillColor($grey);
+		$page->drawRectangle(25, $this->y+20, 570, $this->y);
+		$page->setFillColor($white);
+		$this->_setFontRegular($page, 16);
+		$page->drawText('Shelf', 70, $this->y+4, 'UTF-8');
+		$page->drawText('Qty', 161, $this->y+4, 'UTF-8');
+		$page->drawText('SKU', 220, $this->y+4, 'UTF-8');
+		$page->drawText('Product', 310, $this->y+4, 'UTF-8');
+		$page->drawLine(25, $this->y, 570, $this->y);
 
-		$this->y -=30;
+		$this->y -=22;
 
 		// Here come the ITEMS!
 
@@ -278,7 +284,7 @@ class Mage_Sales_Model_Order_Pdf_PickingList extends Mage_Sales_Model_Order_Pdf_
 				}
 				else
 				{
-					$displayBinLocation = 'Unknown';
+					$displayBinLocation = '?';
 				}
 				// end bin location code.
 					
@@ -287,7 +293,7 @@ class Mage_Sales_Model_Order_Pdf_PickingList extends Mage_Sales_Model_Order_Pdf_
 			}
 			catch (Exception $e)
 			{
-				$Products[$n] = array("BinLocation"=>'Warning',"Qty"=>'SKU',"SKU"=>'Not',"Name"=>'Found');
+				$Products[$n] = array("BinLocation"=>'Error',"Qty"=>'SKU',"SKU"=>'Not',"Name"=>'Found');
 				$n=$n+1;
 			}
 		}
@@ -318,74 +324,113 @@ class Mage_Sales_Model_Order_Pdf_PickingList extends Mage_Sales_Model_Order_Pdf_
 		//how long is it?
 		$numofproducts = count($Products);
 		//write it out then, this is merely drawing the data in the products array
+
 		for ($i = 0; $i < $numofproducts; ++$i)
 		{
+			if ($this->y < 15) {
+				$page = $this->newPageCustom(array('table_header' => true),$order->getRealOrderId());
+			}
+				
 			if(($i%2) == 0)
 			{
-			$page->setFillColor(new Zend_Pdf_Color_GrayScale(0.9));
-			$page->drawRectangle(25, $this->y+23, 570, $this->y-7,Zend_Pdf_Page::SHAPE_DRAW_FILL);
+				$page->setFillColor(new Zend_Pdf_Color_GrayScale(0.9));
+				$page->drawRectangle(25, $this->y+21, 570, $this->y+1,Zend_Pdf_Page::SHAPE_DRAW_FILL);
 			}
-			
-			
-			$this->_setFontRegular($page, 28);
-			
+				
+				
+			$this->_setFontRegular($page, 18);
+				
 			//check box
-			$page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
-			$page->setLineColor(new Zend_Pdf_Color_GrayScale(0.2));
-			$page->drawRectangle(25, $this->y+21, 51, $this->y-5);
-			
-			$page->setFillColor(new Zend_Pdf_Color_GrayScale(0.2));
-			
+			$page->setFillColor($white);
+			$page->setLineColor($darkGrey);
+			$page->drawRectangle(26, $this->y+20, 44, $this->y+2);
+				
+			$page->setFillColor($darkGrey);
+				
 			//color for columns
 			$page->setLineColor(new Zend_Pdf_Color_GrayScale(0.8));
-			
-			$page->drawText($Products[$i]["BinLocation"], 70, $this->y, 'UTF-8');
-			$page->drawLine(150, $this->y+23, 150, $this->y-7);
-			
+				
+			//large bin location logic
+			if ((strlen($Products[$i]["BinLocation"])) <= 4)
+			{
+				$page->drawText($Products[$i]["BinLocation"], 70, $this->y+4, 'UTF-8');
+			}
+			else if ((strlen($Products[$i]["BinLocation"])) <= 46)
+			{
+				$this->_setFontRegular($page, 8);
+
+				$caption = $this->WrapTextToWidth($page, $Products[$i]["BinLocation"], 80);
+				$offset = $this->DrawMultilineText($page, $caption, 50, $this->y+13, 10, 0.2, 10);
+			}
+			else
+			{
+				$this->_setFontRegular($page, 8);
+				$caption = $this->WrapTextToWidth($page, 'Bin location string too long(somehow)', 80);
+				$offset = $this->DrawMultilineText($page, $caption, 50, $this->y+13, 10, 0.2, 10);
+			}
+				
+			$page->drawLine(150, $this->y+21, 150, $this->y);
+				
 			//greater than one logic
 			if($Products[$i]["Qty"]==1)
 			{
-				$this->_setFontRegular($page, 30);
+				$this->_setFontRegular($page, 20);
 				$page->setFillColor(new Zend_Pdf_Color_GrayScale(0.4));
-				$page->drawText($Products[$i]["Qty"], 158, $this->y-2, 'UTF-8');
+				$this->drawTextInBlock($page, $Products[$i]["Qty"], 150, $this->y+3, 60, 22);
 			}
 			else
 			{
-				$this->_setFontBold($page, 30);
-				$page->setFillColor(new Zend_Pdf_Color_GrayScale(0.0));
-				$page->drawText($Products[$i]["Qty"], 158, $this->y-2, 'UTF-8');
+				$this->_setFontBold($page, 20);
+				$page->setFillColor($black);
+				$this->drawTextInBlock($page, $Products[$i]["Qty"], 150, $this->y+3, 60, 22);
 			}
-			$page->drawLine(210, $this->y+23, 210, $this->y-7);
-			
-			//small sku logic
+			$page->drawLine(210, $this->y+21, 210, $this->y);
+				
+			//small sku logic - Outdated thanks to Annette
 			$page->setFillColor(new Zend_Pdf_Color_GrayScale(0.1));
 			if(strlen($Products[$i]["SKU"]) <= 7)
 			{
-			$this->_setFontBold($page, 20);
-			$page->drawText($Products[$i]["SKU"], 220, $this->y, 'UTF-8');
+				$this->_setFontBold($page, 13);
+				$page->drawText(substr($Products[$i]["SKU"], 0, 11), 216, $this->y+5, 'UTF-8');
 			}
 			else
 			{
-			$this->_setFontBold($page, 13);
-			$page->drawText($Products[$i]["SKU"], 220, $this->y, 'UTF-8');				
+				$this->_setFontBold($page, 13);
+				$page->drawText(substr($Products[$i]["SKU"], 0, 11), 216, $this->y+5, 'UTF-8');
 			}
-			$page->drawLine(305, $this->y+23, 305, $this->y-7);
-			
+			$page->drawLine(305, $this->y+21, 305, $this->y);
+				
+			//name changing size
 			$page->setFillColor(new Zend_Pdf_Color_GrayScale(0.3));
-			$this->_setFontRegular($page, 16);
-			$page->drawText($Products[$i]["Name"], 310, $this->y, 'UTF-8');
+			$this->_setFontRegular($page, 12);
+			$actualWidth = 260;
+			$initialWidth = $this->widthForStringUsingFontSize2($Products[$i]["Name"], $page->getFont(), $page->getFontSize());
+
+			if ($initialWidth >= $actualWidth)
+			{
+				$FontSize = $page->getFontSize();
+				$fontType = $page->getFont();
+				$changingString = $Products[$i]["Name"];
+				while ($this->widthForStringUsingFontSize2($changingString, $fontType, $FontSize) >= $actualWidth)
+				{
+					$FontSize -=1;
+				}
+				$this->_setFontRegular($page, $FontSize);
+			}
+				
+			$page->drawText($Products[$i]["Name"], 310, $this->y+6, 'UTF-8');
 				
 			$page->setFillColor(new Zend_Pdf_Color_GrayScale(0.6));
-				
-			$page->setLineColor(new Zend_Pdf_Color_GrayScale(0.5));
-			$page->drawLine(25, $this->y-8, 570, $this->y-8);
-				
-			$this->y -=32;
+
+			$page->setLineColor($grey);
+			$page->drawLine(25, $this->y, 570, $this->y);
+
+			$this->y -=22;
 		}
 
 
 		//some more translation stuff
-		$this->_afterGetPdf();
+
 
 		return $pdf;
 	}
@@ -396,7 +441,7 @@ class Mage_Sales_Model_Order_Pdf_PickingList extends Mage_Sales_Model_Order_Pdf_
 	 * @param array $settings
 	 * @return Zend_Pdf_Page
 	 */
-	public function newPage(array $settings = array())
+	public function newPageCustom(array $settings = array(),$ordernumber)
 	{
 		/* Add new table head */
 		$page = $this->_getPdf()->newPage(Zend_Pdf_Page::SIZE_A4);
@@ -404,25 +449,25 @@ class Mage_Sales_Model_Order_Pdf_PickingList extends Mage_Sales_Model_Order_Pdf_
 		$this->y = 800;
 
 		if (!empty($settings['table_header'])) {
-			$this->_setFontRegular($page);
-			$page->setFillColor(new Zend_Pdf_Color_RGB(0.93, 0.92, 0.92));
-			$page->setLineColor(new Zend_Pdf_Color_GrayScale(0.5));
-			$page->setLineWidth(0.5);
-			$page->drawRectangle(25, $this->y, 570, $this->y-15);
-			$this->y -=10;
-
-			$page->setFillColor(new Zend_Pdf_Color_RGB(0.4, 0.4, 0.4));
-			$page->drawText(Mage::helper('sales')->__('Product'), 35, $this->y, 'UTF-8');
-			$page->drawText(Mage::helper('sales')->__('SKU'), 255, $this->y, 'UTF-8');
-			$page->drawText(Mage::helper('sales')->__('Price'), 380, $this->y, 'UTF-8');
-			$page->drawText(Mage::helper('sales')->__('QTY'), 430, $this->y, 'UTF-8');
-			$page->drawText(Mage::helper('sales')->__('Tax'), 480, $this->y, 'UTF-8');
-			$page->drawText(Mage::helper('sales')->__('Subtotal'), 535, $this->y, 'UTF-8');
-
-			$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
-			$this->y -=20;
+				
+				
+				
+			$this->_setFontRegular($page, 8);
+			$page->setFillColor(new Zend_Pdf_Color_GrayScale(0.1));
+			$page->drawText('#'.$ordernumber, 30, $this->y+20, 'UTF-8');
+			$this->y -=8;
+			//Item Table Headers
+			$page->setFillColor(new Zend_Pdf_Color_GrayScale(0.5));
+			$page->drawRectangle(25, $this->y+20, 570, $this->y);
+			$page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
+			$this->_setFontRegular($page, 16);
+			$page->drawText('Shelf', 70, $this->y+4, 'UTF-8');
+			$page->drawText('Qty', 161, $this->y+4, 'UTF-8');
+			$page->drawText('SKU', 220, $this->y+4, 'UTF-8');
+			$page->drawText('Product', 310, $this->y+4, 'UTF-8');
+			$page->drawLine(25, $this->y, 570, $this->y);
+			$this->y -=22;
 		}
-
 		return $page;
 	}
 
